e86c124fcb229d6b4125e311ac4182aac4c5695f
Nour kayyali <nourkayyali91@gmail.com>
2023-10-05 15:14:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate")
+{
+    return(`This Saturday, I want to ${activity}!`);
+}
+saturdayFun();
+saturdayFun("bathe my dog");
+mondayWork();
+function mondayWork(activity = 'go to the office'){
+    return(`This Monday, I will ${activity}.`);
+}
+wrapAdjective("%")("a dedicated programmer");
+function wrapAdjective(emphatic)
+{
+    
+    if (emphatic===`*`)
+    {
+        const result="a hard worker";
+
+        return function(){
+            return (`You are ${emphatic}${result}${emphatic}!`);
+        }
+    }
+    else if (emphatic===`||`)
+    {
+        const result="a dedicated programmer";
+
+        return function(){
+            return (`You are ${emphatic}${result}${emphatic}!`);
+        }
+    }
+   
+}
